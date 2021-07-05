b69c02f37c1b4550f2ff3172762390bf3a3e5fcc
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-05 16:13:38 +0530

    [Pradip]:ADDED Last Name Validation User Registration System In Regx

--- Regular_Exxpression_Problem_Statements/src/com/bridgelabz/regularexpressions/UserRegistration.java
+++ Regular_Exxpression_Problem_Statements/src/com/bridgelabz/regularexpressions/UserRegistration.java
@@ -1,30 +1,39 @@
    package com.bridgelabz.regularexpressions;
-	import java.util.Scanner;
+    import java.util.Scanner;
 
-	public class UserRegistration {
-		
-//Scanner Class Declared
-		Scanner scan = new Scanner(System.in);
+   public class UserRegistration {
+   	Scanner scan = new Scanner(System.in);
 
-//Declared Method
-		public void verifyFirstName() {
-			System.out.println("Enter First name:");
-			String firstName = scan.next();
-//Check The Conditions 
-			if (firstName.matches("^[A-Z]{1}[a-z]{2,}$") == true)
-				System.out.println("It is a valid First Name");
-			else
-				System.out.println("It is an invalid First Name");
-		}
+   	// Function to verify FirstName and print whether it is valid or not:
 
-		public static void main(String[] args) {
-//Print SOP MSG
-			System.out.println("Welcome to User Registration problem:");
-			
-			//Calling Objects 
-			UserRegistration object = new UserRegistration();
-			object.verifyFirstName();
-		}
+   	public void verifyFirstName() {
+   		System.out.println("Enter First name:");
+   		String firstName = scan.next();
+   		if (firstName.matches("^[A-Z]{1}[a-z]{2,}$") == true)
+   			System.out.println("It is a valid First Name");
+   		else
+   			System.out.println("It is an invalid First Name");
+   		System.out.println("\n");
+   	}
 
-	}
+   
+   	// Function to verify LastName and print whether it is valid or not:
 
+   	public void verifyLastName() {
+   		System.out.println("Enter Last name:");
+   		String lastName = scan.next();
+   		if (lastName.matches("^[A-Z]{1}[a-z]{2,}$") == true)
+   			System.out.println("It is a valid Last Name");
+   		else
+   			System.out.println("It is an invalid Last Name");
+   	}
+
+   	public static void main(String[] args) {
+
+   		System.out.println("Welcome to User Registration problem:");
+   		UserRegistration object = new UserRegistration();
+   		object.verifyFirstName();
+   		object.verifyLastName();
+   	}
+
+   }
