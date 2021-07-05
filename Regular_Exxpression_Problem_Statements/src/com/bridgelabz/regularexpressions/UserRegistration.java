e27f908e6146662984ffd4a286411d48b94c8f90
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-05 17:02:18 +0530

    [Pradip]:ADDED Password Validation At Least One Character
    p]:ADDED Password Validation In User  AT  Least Contain One Character

--- Regular_Exxpression_Problem_Statements/src/com/bridgelabz/regularexpressions/UserRegistration.java
+++ Regular_Exxpression_Problem_Statements/src/com/bridgelabz/regularexpressions/UserRegistration.java
@@ -1,11 +1,12 @@
    package com.bridgelabz.regularexpressions;
   
+
    import java.util.Scanner;
 
    public class UserRegistration {
    	Scanner scan = new Scanner(System.in);
 
-   	// UC-1
+  
    	// Function to verify FirstName and print whether it is valid or not:
 
    	public void verifyFirstName() {
@@ -18,7 +19,7 @@
    		System.out.println("\n");
    	}
 
-   	// UC-2
+   
    	// Function to verify LastName and print whether it is valid or not:
 
    	public void verifyLastName() {
@@ -31,7 +32,7 @@
    		System.out.println("\n");
    	}
 
-   	// UC-3
+   
    	// Function to verify Email and print whether it is valid or not:
 
    	public void verifyEmail() {
@@ -44,7 +45,7 @@
    		System.out.println("\n");
    	}
 
-   	// UC-4
+   
    	// Function to verify Mobile Number and print whether it is valid or not:
 
    	public void verifyMobileNumber() {
@@ -57,13 +58,17 @@
    		System.out.println("\n");
    	}
 
-   	// UC-5
+   	
    	// Function to verify if there are minimum 8 characters in password:
+   	
+   	// Updated Function to verify if there is at least 1 Upper Case Character:
+   	
+   	// Updated Function to verify if there is at least 1 Numeric Character:
 
    	public void verifyPassword() {
    		System.out.println("Enter Password:");
    		String password = scan.next();
-   		if (password.matches("[[a-zA-Z0-9!@#$%^&*]+]{8,}") == true)
+   		if (password.matches("(?=.*[A-Z])(?=.*[0-9])[[a-zA-Z0-9!@#$%^&*]+]{8,}") == true)
    			System.out.println("It is a valid Password");
    		else
    			System.out.println("It is an invalid Password");
@@ -81,4 +86,4 @@
 
    	}
 
-   	   }
+   }
